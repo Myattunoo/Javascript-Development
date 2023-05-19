f768e537c3ebac5c6a1ab8ee64376aed4b82799b
Myat Tun Oo <myattunoo1083@gmail.com>
2023-05-19 10:06:32 +0630

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// var ->before es6
+let name = 'SSE';
+console.log('Hello World');
+
+//Cannot be a reserved keyword
+//Should be meaningful
+//Cannot Start with a number (1name)
+//Cannot Contain a space or hypen (-)
+//Are case-sensitive
+
+let firstName = 'Myat'; //camel notation
+let lastName = 'Oo';  // modern best practicc
+
+//let firstName = 'Myat', let lastName = 'Oo';
+
+let interestRate = 0.3;
+//const interestRate = 0.3;
+interestRate=1;
+console.log(interestRate);
+
+//String  
+let nama ='Mosh';
+//Number 
+ let age=30
+//Boolean 
+let isApproved=true
+//Undefined  
+let firstName2 = undefined
+//Null  
+let selectColor = null;
+
+
